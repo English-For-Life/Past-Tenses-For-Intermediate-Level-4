7965040a2d05a66c11c200b8b1a39dc942e9479a
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-05-09 21:02:10 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,78 @@
+//Array of objects
+const quiz = [
+    {
+        q:"I ____ with friends when someone stole their car.",
+        options:["was staying", "stayed", "had stayed"],
+        answer: 0
+    },
+    {
+     q:"The police asked me where I ___ the night before.",
+        options:["was sleeping", "had slept", "slept"],
+        answer: 1   
+    },
+    {
+        q:"When her brother was in prison, she ___ him every month.",
+        options:["had visited", "was visiting", "visited"],
+        answer: 2
+    },
+    {
+        q:"I ___ her after she had spent six months in prison.",
+        options:["didn't recognise", "wasn't recognising", "hadn't recognised"],
+        answer: 0
+    },
+    {
+        q:"___ you ___ the suspect before you saw her in court ?",
+        options:["Did / meet", "Had / met", "Were / meeting"],
+        answer: 1
+    },
+    {
+        q:"___ the police ___ the men who stole the money yesterday ?",
+        options:["Were / arresting", "Had / arrested", "Did / arrest"],
+        answer: 2
+    },
+    {
+        q:"I had an accident because I ___ at the road.",
+        options:["wasn't looking", "hadn't looked", "didn't look"],
+        answer: 0
+    },
+    {
+        q:"The jury ___ a decision so the judge sent them home.",
+        options:["didn't make", "hadn't made", "weren't making"],
+        answer: 1
+    },
+    {
+        q:"I ___ in the park when I saw a crowd of people.",
+        options:["played", "had played", "was playing"],
+        answer: 2
+    },
+    {
+        q:"The roads were dangerous because it ___ in the morning.",
+        options:["had snowed", "snowed", "was snowing"],
+        answer: 0
+    },
+    {
+        q:"Everyone was driving slowly because they ___ the accident.",
+        options:["saw", "had seen", "were seeing"],
+        answer: 1
+    },
+    {
+     q:"When we got to the court the judge ___ the criminals.",
+        options:["sentenced", "was sentenced", "had sentenced"],
+        answer: 2   
+    },
+    {
+        q:"She ___ in the bank every day for ten years.",
+        options:["worked", "was worked", "had worked"],
+        answer: 0
+    },
+    {
+        q:"They were in prison because they ___ serious crimes.",
+        options:["committed", "had committed", "were committing"],
+        answer: 1
+    },
+    {
+        q:"The shoplifter confessed as soon as the police ___ her.",
+        options:["arrested", "were arresting", "had arrested"],
+        answer: 2
+    }
+]
